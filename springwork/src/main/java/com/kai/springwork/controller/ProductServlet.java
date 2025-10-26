33eb41317d8f2825e87674bbe0a22543ebc32c09
water <s23078997@gmail.com>
2025-10-27 03:13:37 +0800

    商品資訊 -

--- springwork/src/main/java/com/kai/springwork/controller/ProductServlet.java
+++ springwork/src/main/java/com/kai/springwork/controller/ProductServlet.java
@@ -8,6 +8,7 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 
 
 import java.io.IOException;
@@ -17,8 +18,8 @@ import java.util.List;
 @WebServlet("/getProducts")
 public class ProductServlet extends HttpServlet {
 
-
-    private ProductDao productDao = new ProductDaoImpl();
+    @Autowired
+    private ProductDao productDao;
 
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response)
