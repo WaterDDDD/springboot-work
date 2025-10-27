29e40d29c863d6b2c4020c50af0b1fe7feb60f45
water <s23078997@gmail.com>
2025-10-27 14:55:47 +0800

    商品資訊 - DAO

--- springwork/src/main/java/com/kai/springwork/dao/impl/ProductDaoImpl.java
+++ springwork/src/main/java/com/kai/springwork/dao/impl/ProductDaoImpl.java
@@ -1,44 +1,34 @@
 package com.kai.springwork.dao.impl;
 
 
-import com.kai.springwork.constant.ProductCategory;
 import com.kai.springwork.dao.ProductDao;
 import com.kai.springwork.model.Product;
+import com.kai.springwork.rowmapper.ProductRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
 
-import java.sql.*;
-import java.util.ArrayList;
+import org.springframework.stereotype.Component;
+
+
+
 import java.util.List;
 
+@Component
 public class ProductDaoImpl implements ProductDao {
 
-    private String jdbcURL = "jdbc:mysql://localhost:3306/work?serverTimezone=Asia/Taipei&characterEncoding=utf-8";
-    private String jdbcUsername = "root";
-    private String jdbcPassword = "springboot";
+    private final JdbcTemplate jdbcTemplate;
+
+    public ProductDaoImpl(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
 
     @Override
     public List<Product> getProduct(String keyword) {
 
-        {
+        String sql = "SELECT product_name, category, price, stock FROM commodity " +
+                "WHERE product_name LIKE ? OR category LIKE ?";
+        String search = "%" + keyword + "%";
 
-            List<Product> products = new ArrayList<>();
-            String sql = "SELECT product_name, category, price, stock FROM commodity " +
-                    "WHERE product_name LIKE ? OR category LIKE ?";
+        return jdbcTemplate.query(sql, new Object[]{search, search}, new ProductRowMapper());
 
-
-            try (Connection connection = DriverManager.getConnection(jdbcURL, jdbcUsername, jdbcPassword);
-                 PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
-
-                String keyname = "%" + keyword + "%";
-                preparedStatement.setString(1, keyname);
-                preparedStatement.setString(2, keyname);
-
-
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-
-            return products;
-
-        }
     }
 }
