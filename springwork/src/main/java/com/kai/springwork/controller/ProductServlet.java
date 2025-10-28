f9fedc0a793ec72d181086ef013c1178f2182b51
water <s23078997@gmail.com>
2025-10-28 19:29:22 +0800

    商品資訊 - Servlet

--- springwork/src/main/java/com/kai/springwork/controller/ProductServlet.java
+++ springwork/src/main/java/com/kai/springwork/controller/ProductServlet.java
@@ -1,14 +1,16 @@
 package com.kai.springwork.controller;
 
 import com.kai.springwork.dao.ProductDao;
-import com.kai.springwork.dao.impl.ProductDaoImpl;
+
 import com.kai.springwork.model.Product;
+import jakarta.servlet.ServletContext;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
 
 
 import java.io.IOException;
@@ -18,8 +20,17 @@ import java.util.List;
 @WebServlet("/getProducts")
 public class ProductServlet extends HttpServlet {
 
-    @Autowired
     private ProductDao productDao;
+
+    @Override
+    public void init() throws ServletException {
+
+        ServletContext servletContext = getServletContext();
+        ApplicationContext context =
+                WebApplicationContextUtils.getWebApplicationContext(servletContext);
+
+        productDao = context.getBean(ProductDao.class);
+    }
 
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response)
